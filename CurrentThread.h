a6f73c45c45cde5bcc268201bd56c24bbe02b914
ericps <1499273524@qq.com>
2023-07-15 15:05:52 +0000

    comment __thread

--- CurrentThread.h
+++ CurrentThread.h
@@ -6,7 +6,9 @@
 // 获取当前线程 id
 namespace CurrentThread {
 
-//!TODO: __thread 是 thread_local 机制
+// C++11 thread_local
+//!NOTE: __thread是GCC内置的线程局部存储设施。_thread变量每一个线程有一份独立实体，各个线程的值互不干扰。
+// 可以用来修饰那些带有全局性且值可能变，但是又不值得用全局变量保护的变量
 extern __thread int t_cachedTid;
 
 void cacheTid();
